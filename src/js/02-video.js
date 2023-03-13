432c22c70128357899c6c34632af6dffc2684f44
Dmytro Hubin <dimkanishe2099@gmail.com>
2023-03-13 02:33:50 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,12 +5,17 @@ const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const CURRENT_TIMECODE = 'videoplayer-current-time';
 
-if (localStorage.getItem(CURRENT_TIMECODE)) {
-  player.setCurrentTime(localStorage.getItem(CURRENT_TIMECODE));
-}
+setCurrentTime();
 
 const onPlay = function (data) {
   localStorage.setItem(CURRENT_TIMECODE, JSON.stringify(data.seconds));
 };
 
 player.on('timeupdate', throttle(onPlay, 2000));
+
+function setCurrentTime() {
+  if (!localStorage.getItem(CURRENT_TIMECODE)) {
+    return;
+  }
+  player.setCurrentTime(localStorage.getItem(CURRENT_TIMECODE));
+}
